46b2d6bb36bfe219965ca5715585974b02fd47d2
Ömer ÖZDEMİR <ozd.omer17@gmail.com>
2025-08-03 17:10:30 +0300

    commit

---
+++ .prettierrc.js
@@ -0,0 +1,6 @@
+export const singleQuote = true;
+export const printWidth = 80;
+export const tabWidth = 2;
+export const trailingComma = 'es5';
+export const bracketSpacing = true;
+export const arrowParens = 'always';
